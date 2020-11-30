04f4c1a2d8779003b68478fd649f42deb79b6b5f
liandi <simdigliandi@gmail.com>
2020-11-30 15:52:49 +0700

    update

---
+++ index.js
@@ -0,0 +1,18 @@
+var app = require("express")();
+var socket = require("socket.io");
+
+const server = app.listen(5000, function () {
+  console.log("server is running on port 5000");
+});
+
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  socket.on("SEND_MESSAGE", function (data) {
+    io.emit("RECEIVE_MESSAGE", data);
+  });
+
+  socket.on("disconnect", () => {
+    socket.leave(socket.id);
+  });
+});
